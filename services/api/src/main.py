08ec63a0aa02bc2ca3d108feac951a802765fe82
Matthew Hall <matthewhall@Matthews-MacBook-Air.local>
2023-07-18 17:17:05 +0100

    Refactored code to outside

--- services/api/src/main.py
+++ services/api/src/main.py
@@ -4,11 +4,11 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+data = requests.get("http://135.181.118.171:7070/items/0").json()
+
 @app.get("/")
 def read_root(request: Request):
-
     print(request.headers)
-    data = requests.get("http://135.181.118.171:7070/items/0").json()
     random_number = random.randint(0, len(data))
     return data[random_number]['item_key']
 
