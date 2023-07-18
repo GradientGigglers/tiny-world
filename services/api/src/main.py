6744c364841c728955182f8d4020cd9b7ae36482
adam gilbert <spacetimedecor@gmail.com>
2023-07-18 14:59:41 +0100

    test change

--- services/api/src/main.py
+++ services/api/src/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root(request: Request):
     print(request.headers)
-    return {"Hello": "From TinyWorld Again"}
+    return {"Hello": "From TinyWorld Again Again"}
